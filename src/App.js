1293ce85f7d0c0f85390998a4a885c58501bb5b8
HJhjhjjhhjj <brotherj1793@naver.com>
2024-05-09 11:42:47 +0900

    front setting

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Routes } from 'react-router-dom';
+import Layout from "./components/layouts/Layout";
+
+function App() {
+  return (
+
+      <Layout>
+        <Routes>
+        </Routes>
+      </Layout>
+
+  );
+}
+
+export default App;
---
+++ src/components/layouts/Layout.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+import Main from "./Main";
+
+const Layout = ({ children }) => {
+    return (
+        <BrowserRouter>
+            <Main>
+                {children}
+            </Main>
+        </BrowserRouter>
+    );
+}
+
+export default Layout;
