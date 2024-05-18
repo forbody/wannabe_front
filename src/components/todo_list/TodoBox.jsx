b3a4ace7d9541c46dace814160bdb6d98d00a6b3
Choiseunggwan <159886490+Choiseunggwan@users.noreply.github.com>
2024-05-18 09:47:26 +0900

    conflict 수정

--- src/components/todo_list/TodoBox.jsx
+++ src/components/todo_list/TodoBox.jsx
@@ -5,25 +5,52 @@ import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import { useEffect, useState } from "react";
 import TodoEle from "./TodoEle";
 
-const TodoBox = () => {
+const TodoBox = ({food}) => {
     const [isTrue, setIsTrue] = useState(false);
     
     const onIsTrue = () => {
         setIsTrue(!isTrue)
     }
-    return (  
-        <ForegroundBox  style={{backgroundColor : '#ffffff99', width : '300px', flexDirection:'column', marginTop:'10px'}} >
-            <Grid container spacing={0} alignItems={'center'} style={{fontSize:'20px', fontWeight: 'bold'}}>
-                <Grid item xs={8}>식단</Grid>
-                <Grid item xs={2} sx={{textAlign :'center'}} >3/4</Grid>
-                <Grid item xs={2} sx={{textAlign :'center'}}  onClick={()=>onIsTrue()}>
-                    <IconButton sx={{margin :'0', padding : '0'}}>
-                        {isTrue?<ExpandLessIcon fontSize="large" />:<ExpandMoreIcon fontSize="large" />}
+    const total = food?.length;
+    const achieve = food?.filter((e) => e.achieve === true).length;
+    return (
+        <ForegroundBox
+            style={{
+                backgroundColor: "#ffffff99",
+                width: "300px",
+                flexDirection: "column",
+                marginTop: "10px",
+            }}
+        >
+            <Grid
+                container
+                spacing={0}
+                alignItems={"center"}
+                style={{ fontSize: "20px", fontWeight: "bold" }}
+            >
+                <Grid item xs={8}>
+                    식단
+                </Grid>
+                <Grid item xs={2} sx={{ textAlign: "center" }}>
+                    {achieve} / {total}
+                </Grid>
+                <Grid
+                    item
+                    xs={2}
+                    sx={{ textAlign: "center" }}
+                    onClick={() => onIsTrue()}
+                >
+                    <IconButton sx={{ margin: "0", padding: "0" }}>
+                        {isTrue ? (
+                            <ExpandLessIcon fontSize="large" />
+                        ) : (
+                            <ExpandMoreIcon fontSize="large" />
+                        )}
                     </IconButton>
                 </Grid>
             </Grid>
             {/* 여기서 엘리먼트 반복추가하면될듯 */}
-            {isTrue?<TodoEle />:false}
+            {isTrue ? <TodoEle food={food} /> : false}
         </ForegroundBox>
     );
 }
