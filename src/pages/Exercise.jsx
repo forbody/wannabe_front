cdd8db5283f01000e63880615fed76e100e25713
Choiseunggwan <159886490+Choiseunggwan@users.noreply.github.com>
2024-05-24 17:56:16 +0900

    기능추가 : exercise 셀럽 사진 추가

--- src/pages/Exercise.jsx
+++ src/pages/Exercise.jsx
@@ -8,6 +8,7 @@ import ExerciseFollow from "../components/exercises/ExerciseFollow";
 import { useAuth } from './../hooks/useAuth';
 import Carousel from "react-material-ui-carousel";
 import ExerciseModal from "../components/exercises/ExerciseModal";
+import GetUserandRoleModel from "../components/user/GetUserandRoleModel";
 
 const Exercise = () => {
     const { loginUser } = useAuth();
@@ -15,6 +16,7 @@ const Exercise = () => {
     const [exercises, setExercises] = useState([]);
     const [randTip, setRandTip] = useState();
     const [favExercises, setFavExercises] = useState([]);
+    const { modelImg } = GetUserandRoleModel();
 
     const getExercises = async () => {
         const res = await axios.get('http://localhost:8000/v1/exercise');
@@ -45,24 +47,29 @@ const Exercise = () => {
     }, [refreshFav]);
     // 해당 유저가 즐겨찾기한 운동 목록 조회
 
-    return ( 
+    return (
         <>
             <h1>운동</h1>
             <Box
                 sx={{
-                    display: 'flex',
-                    width: '100%',
-                    flexWrap: 'wrap',
-                    overflowY: 'scroll',
-                    scrollbarWidth: 'none',
-                    justifyContent: 'center',
-                    alignContent: 'flex-start'
+                    display: "flex",
+                    width: "100%",
+                    flexWrap: "wrap",
+                    overflowY: "scroll",
+                    scrollbarWidth: "none",
+                    justifyContent: "center",
+                    alignContent: "flex-start",
                 }}
             >
                 <BackgroundBox half>
-                    <h3 >
-                    셀럽 이미지
-                    </h3>
+                    {modelImg && (
+                        <img
+                            src={`http://localhost:8000/${modelImg}`}
+                            width="100%"
+                            alt={"img"}
+                            style={{ borderRadius: "20px" }}
+                        />
+                    )}
                 </BackgroundBox>
                 <BackgroundBox half>
                     헬스 팁 - {randTip?.health_tip}
@@ -71,32 +78,35 @@ const Exercise = () => {
                 <BackgroundBox>
                     <h3>내가 즐겨찾기 한 운동</h3>
                     <br />
-                    <Carousel 
+                    <Carousel
                         showArrows={false}
                         autoPlay={false}
                         infiniteLoop={true}
                         showThumbs={false}
                         swipe={true}
                     >
-                                {
-                                    favExercises && favExercises.map(f => (
-                                        <ExerciseFollow favExercise={f} />
-                                    ))
-                                }
+                        {favExercises &&
+                            favExercises.map((f) => (
+                                <ExerciseFollow favExercise={f} />
+                            ))}
                     </Carousel>
                 </BackgroundBox>
                 <BackgroundBox>
                     운동 목록
                     <ExerciseSelect />
-                    
                 </BackgroundBox>
                 <BackgroundBox>
                     <>
-                    {
-                        favExercises && exercises.map(e => (
-                            <ExerciseDetail key={e.id} exercise={e} favExercises={favExercises} refreshFav={refreshFav} setRefreshFav={setRefreshFav} /> //3 
-                        ))
-                    }
+                        {favExercises &&
+                            exercises.map((e) => (
+                                <ExerciseDetail
+                                    key={e.id}
+                                    exercise={e}
+                                    favExercises={favExercises}
+                                    refreshFav={refreshFav}
+                                    setRefreshFav={setRefreshFav}
+                                /> //3
+                            ))}
                     </>
                 </BackgroundBox>
             </Box>
