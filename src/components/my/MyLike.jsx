f8847bb66f96db6822ae73ef5fd49066de34167c
Hae8 <alt9ir@gmail.com>
2024-05-25 15:34:30 +0900

    좋아요 확인 기능 추가

--- src/components/my/MyLike.jsx
+++ src/components/my/MyLike.jsx
@@ -1,7 +1,45 @@
 import { Box, Button, ButtonGroup, Divider, Typography } from "@mui/material";
 import { ForegroundBox } from "../styled_comp/StyledDiv";
+import { userApi } from "../../api/services/user";
+import GetUserandRoleModel from "../user/GetUserandRoleModel";
+import { useAuth } from "../../hooks/useAuth";
+import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
 
 const MyLike = () => {
+    const { loginUser } = useAuth()
+    const { userProfile } = GetUserandRoleModel();
+    /*
+    const [liker, setLiker] = useState();
+
+    const getLikings = async () => {
+        try{
+            const res = await userApi.getLikers(`${userProfile?.id}`, loginUser)
+            if (res.code === 200) {
+                console.log('좋아하는 사람 가져오기 성공');
+                setLiker(res.payload)
+            } else {
+                throw new Error(res.message);
+            }
+        }catch(err) {
+            Swal.fire({
+                title: "에러 발생",
+                text: err.message,
+                icon: "error"
+            });
+        }
+    }
+
+    useEffect(() => {
+        getLikings();
+    }, [loginUser, userProfile]);
+    */
+
+    // 아직 userProfile을 못 가져온 상태처리
+    if (!userProfile) {
+        return <div>Loading...</div>;
+    } 
+
     return (
         <ForegroundBox
         display='flex'
