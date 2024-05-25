f3bb90d6e1a0b2047ff67d71fc3c6008b99e8c74
Choiseunggwan <159886490+Choiseunggwan@users.noreply.github.com>
2024-05-25 11:46:35 +0900

    기능추가 : todoApi

--- src/components/todo_list/TodoBoxFood.jsx
+++ src/components/todo_list/TodoBoxFood.jsx
@@ -11,6 +11,11 @@ const TodoBoxFood = ({ element, setIsAchieve, children }) => {
     const [breakfast, setBreakfast] = useState();
     const [lunch, setLunch] = useState();
     const [dinner, setDinner] = useState();
+    const [breakfastCal, setBreakfastCal] = useState();
+    const [lunchCal, setLunchCal] = useState();
+    const [dinnerCal, setDinnerCal] = useState();
+    
+    console.log(lunch && lunch);
     const onIsTrue = () => {
         setIsTrue(!isTrue);
     };
@@ -24,6 +29,16 @@ const TodoBoxFood = ({ element, setIsAchieve, children }) => {
         setLunch(element.filter((e) => e.order == 2));
         setDinner(element.filter((e) => e.order == 3));
     },[element])
+
+    // useEffect(() => {
+    //     setBreakfastCal(breakfast?.reduce((acc, e) => acc + e.Food[0]?.calory, 0));
+    // },[breakfast])
+    useEffect(() => {
+        setLunchCal(lunch?.reduce((acc, e) => acc + e.Food[0]?.calory, 0));
+    },[lunch])
+    // useEffect(() => {
+    //     setDinnerCal(dinner.reduce((acc, e) => acc + e.Food[0]?.calory, 0));
+    // },[dinner])
 
     return (
         <ForegroundBox
@@ -62,64 +77,95 @@ const TodoBoxFood = ({ element, setIsAchieve, children }) => {
             </Grid>
             {isTrue ? (
                 <>
-                    {breakfast.length?
-                    <>
-                        <Box
-                            sx={{
-                                borderBottom: "1px solid black",
-                                textAlign: "center",
-                                fontSize: "16px",
-                                fontWeight: "bold",
-                            }}
-                        >
-                            아침
-                        </Box>
-                        {breakfast?.map((e) => (
-                            <TodoEle e={e} key={e.id} setIsAchieve={setIsAchieve} />
-                        ))}
-                    </>
-                    :
-                    false
-                    }
-                    {lunch.length ?
-                    <>
-                        <Box
-                            sx={{
-                                borderBottom: "1px solid black",
-                                textAlign: "center",
-                                fontSize: "16px",
-                                fontWeight: "bold",
-                            }}
-                        >
-                            점심
-                        </Box>
-                        {lunch?.map((e) => (
-                            <TodoEle e={e} key={e.id} setIsAchieve={setIsAchieve} />
-                        ))}
-                        
-                    </>
-                    :
-                    false
-                    }
-                    {dinner.length?
-                    <>
-                        <Box
-                            sx={{
-                                borderBottom: "1px solid black",
-                                textAlign: "center",
-                                fontSize: "16px",
-                                fontWeight: "bold",
-                            }}
-                        >
-                            저녁
-                        </Box>
-                        {dinner?.map((e) => (
-                            <TodoEle e={e} key={e.id} setIsAchieve={setIsAchieve} />
-                        ))}
-                    </>
-                    :
-                    false
-                    }
+                    {breakfast.length ? (
+                        <>
+                            <Box
+                                sx={{
+                                    borderBottom: "1px solid black",
+                                    textAlign: "center",
+                                    fontSize: "16px",
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                아침
+                            </Box>
+                            {breakfast?.map((e) => (
+                                <TodoEle
+                                    e={e}
+                                    key={e.id}
+                                    setIsAchieve={setIsAchieve}
+                                />
+                            ))}
+                            <Box
+                                sx={{
+                                    borderBottom: "1px solid black",
+                                    textAlign: "center",
+                                    fontSize: "16px",
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                아침
+                            </Box>
+                        </>
+                    ) : (
+                        false
+                    )}
+                    {lunch.length ? (
+                        <>
+                            <Box
+                                sx={{
+                                    borderBottom: "1px solid black",
+                                    textAlign: "center",
+                                    fontSize: "16px",
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                점심
+                            </Box>
+                            {lunch?.map((e) => (
+                                <TodoEle
+                                    e={e}
+                                    key={e.id}
+                                    setIsAchieve={setIsAchieve}
+                                />
+                            ))}
+                            <Box
+                                sx={{
+                                    borderBottom: "1px solid black",
+                                    textAlign: "center",
+                                    fontSize: "16px",
+                                    color: 'primary'
+                                }}
+                            >
+                                {lunchCal} kcal
+                            </Box>
+                        </>
+                    ) : (
+                        false
+                    )}
+                    {dinner.length ? (
+                        <>
+                            <Box
+                                sx={{
+                                    borderBottom: "1px solid black",
+                                    textAlign: "center",
+                                    fontSize: "16px",
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                저녁
+                            </Box>
+                            {dinner?.map((e) => (
+                                <TodoEle
+                                    e={e}
+                                    key={e.id}
+                                    setIsAchieve={setIsAchieve}
+                                />
+                            ))}
+                        </>
+                    ) : (
+                        false
+                    )}
                 </>
             ) : (
                 false
