49dd45f3bee5ba3be107d75d7d300e6ff60aebc0
Hae8 <alt9ir@gmail.com>
2024-05-29 15:26:05 +0900

    dayAchieve 컴포넌트 분리

--- src/components/my/DayAchive.jsx
+++ src/components/my/DayAchive.jsx
@@ -1,35 +1,25 @@
 import { Typography } from "@mui/material";
 import { ForegroundBox } from "../styled_comp/StyledDiv";
 
-const DayAchieve = ({goal}) => {
-    const today1 = new Date()
-    const today_month = today1.getMonth()+1;
-    const today_date = today1.getDate();
+const DayAchieve = ({date, goal}) => {
 
     return (
         <ForegroundBox
-        display='flex'
-        style={{
-            width:'100%',
-            flexDirection: 'column',
-            justifyContent: 'space-evenly',
-            alignItems:'center'
-        }}
-        >
-            <Typography
-            variant='h6'
-            fontWeight='600'
+                display="flex"
+                style={{
+                    width: "100%",
+                    flexDirection: "column",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                }}
             >
-                {today_month}월 {today_date}일의 달성도
-            </Typography>
-            <Typography
-            variant='h6'
-            fontWeight='600'
-            color='secondary'
-            >
-                { goal ? goal : 0} %
-            </Typography>
-        </ForegroundBox>
+                <Typography variant="h6" fontWeight="600">
+                    {date.slice(5, 7)}월 {date.slice(8, 10)}일의 달성도
+                </Typography>
+                <Typography variant="h6" fontWeight="600" color="secondary">
+                    {goal ? goal : 0} %
+                </Typography>
+            </ForegroundBox>
     );
 }
 
--- src/components/my/MyCalendar.jsx
+++ src/components/my/MyCalendar.jsx
@@ -64,22 +64,7 @@ const MyCalendar = () => {
 
     return (
         <>
-            <ForegroundBox
-                display="flex"
-                style={{
-                    width: "100%",
-                    flexDirection: "column",
-                    justifyContent: "space-evenly",
-                    alignItems: "center",
-                }}
-            >
-                <Typography variant="h6" fontWeight="600">
-                    {date.slice(5, 7)}월 {date.slice(8, 10)}일의 달성도
-                </Typography>
-                <Typography variant="h6" fontWeight="600" color="secondary">
-                    {goal ? goal : 0} %
-                </Typography>
-            </ForegroundBox>
+            <DayAchieve date={date} goal={goal}/>
             <ForegroundBox
                 display="flex"
                 style={{
